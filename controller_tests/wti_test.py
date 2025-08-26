8ed945e93b71a2f6a3cd1a6a73de203f9654e126
Carlos Huaccho - Network to Code <chuacch@anstelsbx01.ipaper.com>
2025-08-26 10:27:27 -0500

    fixing wti tests

--- controller_tests/wti_test.py
+++ controller_tests/wti_test.py
@@ -1,8 +1,9 @@
+import json
 from base64 import b64encode
 from os import environ
 from pathlib import Path
 
-import requests
+from requests import request
 import urllib3
 from dotenv import load_dotenv
 
@@ -32,13 +33,22 @@ def base_64_encode_credentials(username: str, password: str) -> str:
     return f"Basic {b64encode(s=credentials_str).decode(encoding='utf-8')}"
 
 
-def WTI():
+hostname_put: dict[str, str] = {"unitid": {
+        "timestamp": "2025-08-20T15:10:49+00:00",
+        "siteid": "02B - Memphis Test Lab, TN",
+        "location": "02B_Memphis Test Lab_TN",
+        "assettag": "",
+        "hostname": "02B-WTI-OOB-LAB-1",
+        "domain": "ipaper.com",
+},}
+
+def WTI1():
     # Address of the WTI device
     URI = "https://"
     SITE_NAME = "164.103.40.67"
 
     # put in the username and password to your WTI device here
-    BASE_PATH = "/api/v2/config/snmpaccess"
+    BASE_PATH = "/api/v2/config/hostname"
     encoded_creds = base_64_encode_credentials(
         username=environ["WTI_USERNAME"],
         password=environ["WTI_PASSWORD"],
@@ -48,37 +58,46 @@ def WTI():
         "Content-Type": "application/json",
     }
 
-    x = requests.get(
+    x = request(
         url=URI + SITE_NAME + BASE_PATH,
+	method="GET",
         verify=False,
         headers=HEADER,
-        params="ports=eth0",
+	# data=hostname_put
+        # params="ports=eth0",
     )
-    print(x.text)
+    print(json.dumps(x.json(), indent=4))
 
 
-# def WTI2():
-#     # Address of the WTI device
-#     URI = "https://"
-#     SITE_NAME = "164.103.40.67"
+def WTI2():
+    # Address of the WTI device
+    URI = "https://"
+    SITE_NAME = "164.103.40.67"
 
-#     # put in the username and password to your WTI device here
-#     BASE_PATH = "/api/v2/config/aaaserver"
-#     USERNAME = "username"
-#     PASSWORD = "password"
-#     AUTH = (USERNAME, PASSWORD)
-#     HEADER = ""
+    # put in the username and password to your WTI device here
+    BASE_PATH = "/api/v2/config/aaaserver"
+    encoded_creds = base_64_encode_credentials(
+        username=environ["WTI_USERNAME"],
+        password=environ["WTI_PASSWORD"],
+    )
 
-#     x = requests.get(
-#         URI + SITE_NAME + BASE_PATH,
-#         verify=False,
-#         auth=AUTH,
-#         headers=HEADER,
-#         params="service=tacacs",
-#     )
-#     print(x.text)
+    HEADER = {
+        "Authorization": encoded_creds,
+        "Content-Type": "application/json",
+    }
+
+    x = request(
+        url=URI + SITE_NAME + BASE_PATH,
+	method="GET",
+        verify=False,
+        headers=HEADER,
+        params="service=tacacs",
+    )
+    with open("aaa_resp.json", mode="w", encoding="utf-8") as file:
+        json.dump(x.json(), file, indent=4)
+    print(json.dumps(x.json(), indent=4))
 
 
 if __name__ == "__main__":
     # get_devices()
-    WTI()
+    WTI2()
